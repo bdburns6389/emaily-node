cc6eceeb38ce88a44e7b9f9fbfd8373724020ecd
Brian Burns <bdburns6389@gmail.com>
2018-05-20 22:24:26 -0500

    added PORT inside listen function

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+ //Gives port for Heroku to apply PORT, or uses 5000 for development
+app.listen(proccess.env.PORT || 5000);
