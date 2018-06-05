f47e0c3b3d636548311ea0272a6ad4a81deadd96
bdburns6389 <bdburns6389@gmail.com>
2018-06-04 22:55:59 -0500

    Corrected Callback URI in GoogleAPI

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-        console.log(accessToken);
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
 );
 
 const PORT = process.env.PORT || 5000;
- //Gives port for Heroku to apply PORT, or uses 5000 for development
+//Gives port for Heroku to apply PORT, or uses 5000 for development
 app.listen(PORT);
