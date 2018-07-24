9310fd4daefcbf340552e1a470f52cff66c73b91
bdburns6389 <bdburns6389@gmail.com>
2018-07-23 23:23:41 -0500

    Beginning implementing SurveyForm and dry'ing up code using lodash

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,49 @@
+// SurveyForm shows a form for user to add input
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button
+            style={{ color: "white" }}
+            className="deep-purple darken-4"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
