fd1fe6155dd7ca2469c36526c8e393581580099d
bdburns6389 <bdburns6389@gmail.com>
2018-08-07 19:10:02 -0500

    Added GET route for surveyRoutes to return specific surveys from user.

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,8 +1,7 @@
-# For linux and MacOS replace webhook in package.json to webhook : ./sendgrid_webhook.sh
-localtunnel () {
-  lt -s emaily-bdburns6389 --port 5000
+function localtunnel {
+  lt -s emailybdburns6389 --port 5000
 }
 until localtunnel; do
-  echo "localtunnel server crashed"
-  sleep 2
+echo "localtunnel server crashed"
+sleep 2
 done
